c91c974408bb49efab4ff00e9031daca87d193ce
Manuel Nicolás <mediavilla.grosjean@gmail.com>
2024-05-10 04:20:42 -0300

    Agregar clase UsuarioVistaModelo a la capa de Presentacion

---
+++ Presentacion/UsuarioVistaModelo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace Presentacion
+{
+    public class UsuarioVistaModelo : IUsuarioVistaModelo
+    {
+        private Usuario _usuario;
+
+        public UsuarioVistaModelo(Usuario usuario)
+        {
+            _usuario = usuario;
+        }
+
+        public string UserName => _usuario.UserName;
+
+        public string RolPerfil => _usuario.Perfil?.Rol ?? string.Empty;
+    }
+}
