e961af9f7eb2f6a8f9a00a9db93f59ef225e0314
Manuel Nicolás <mediavilla.grosjean@gmail.com>
2024-05-10 04:18:35 -0300

    Agregar clase UsuarioNegocio a la capa de Negocio

---
+++ Negocio/UsuarioNegocio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using Repositorio;
+
+namespace Negocio
+{
+    public class UsuarioNegocio
+    {
+        private UsuarioRepositorio _usuarioRepositorio;
+        public UsuarioNegocio()
+        {
+            _usuarioRepositorio = new UsuarioRepositorio();
+        }
+
+        // Falta implementar
+        public bool CrearUsuario(Usuario usuario)
+        {
+            return false;
+        }
+
+        public List<Usuario> GetUsuarios()
+        {
+            return _usuarioRepositorio.GetUsuarios();
+        }
+
+    }
+}
