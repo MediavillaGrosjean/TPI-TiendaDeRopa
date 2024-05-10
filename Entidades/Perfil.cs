6b3e163f20e2aac604057f4729d7c5151bde1458
Manuel Nicolás <mediavilla.grosjean@gmail.com>
2024-05-10 03:47:41 -0300

    Agregar clase Perfil a la capa de Entidades

---
+++ Entidades/Perfil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Perfil : IEntidad
+    {
+        private int _id;
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private string _rol;
+        public string Rol
+        {
+            get { return _rol; }
+            set { _rol = value; }
+        }
+    }
+}
