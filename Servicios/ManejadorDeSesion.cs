03ed96e108d84386ce50cae7d650283577811df8
Manuel Nicolás <mediavilla.grosjean@gmail.com>
2024-05-10 03:54:49 -0300

    Agregar clase ManejadorDeSesion a la capa de Servicios

---
+++ Servicios/ManejadorDeSesion.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace Servicios
+{
+   public class ManejadorDeSesion
+    {
+        private static ManejadorDeSesion _instancia;
+        private Usuario _sesion;
+
+        private ManejadorDeSesion() { }
+
+        public static ManejadorDeSesion Instancia
+        {
+            get
+            {
+                if (_instancia == null)
+                {
+                    _instancia = new ManejadorDeSesion();
+                }
+                return _instancia;
+            }
+        }
+
+        public Usuario Sesion
+        {
+            get { return _sesion; }
+            set { _sesion = value; }
+        }
+
+        public void LogIn(Usuario usuario)
+        {
+            _sesion = usuario;
+        }
+
+        public void LogOut()
+        {
+            _sesion = null;
+        }
+
+        public bool IsLogged()
+        {
+            return _sesion != null;
+        }
+    }
+}
