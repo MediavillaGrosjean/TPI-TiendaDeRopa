75d65680b703912937a31ab333ec97fa680d1336
Manuel Nicolás <mediavilla.grosjean@gmail.com>
2024-05-10 04:20:04 -0300

    Agregar interfaz IUsuarioVistaModelo a la capa de Presentacion

---
+++ Presentacion/IUsuarioVistaModelo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentacion
+{
+    public interface IUsuarioVistaModelo
+    {
+        string UserName { get; }
+        string RolPerfil { get; }
+    }
+}
